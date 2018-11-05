0d211300196ab1800d2495307cc8cb7ff5ee9205
Antonio Ribeiro <antonio.ribeiro@mercedes-benz.io>
2018-11-05 15:08:15 +0000

    Initial commit of Open API Collector

---
+++ open-api-aggregator/src/main/java/pt/devhub/antjori/cicd/oac/Application.java
@@ -0,0 +1,16 @@
+package pt.devhub.antjori.cicd.oac;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * The bootstrap (base) application that will trigger Spring Boot.
+ */
+@SpringBootApplication(scanBasePackages = "pt.devhub.antjori.cicd.oag")
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+
+}
---
+++ open-api-aggregator/src/main/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorController.java
@@ -0,0 +1,11 @@
+package pt.devhub.antjori.cicd.oac.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for the Open API Collector.
+ */
+@RestController
+public class OpenApiCollectorController {
+
+}
