f2894f1497074d692c15850bac19f506c0fdffd7
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-11-20 23:35:07 +0000

    Add missing Spotify track attributes

--- src/main/java/pt/devhub/antjori/cicd/oac/model/spotify/SpotifyTrack.java
+++ src/main/java/pt/devhub/antjori/cicd/oac/model/spotify/SpotifyTrack.java
@@ -61,10 +61,19 @@ public class SpotifyTrack extends SpotifyElement {
     private ExternalIDs externalIDs;
 
     /**
-     * Whether or not the track is from a local file.
+     * Part of the response when Track Relinking is applied. If true , the track is
+     * playable in the given market. Otherwise false.
      */
-    @JsonProperty(value = "is_local")
-    private boolean isLocal;
+    @JsonProperty(value = "is_playable")
+    private boolean isPlayable;
+
+    /**
+     * Part of the response when Track Relinking is applied, and the requested track
+     * has been replaced with different track. The track in the linked_from object
+     * contains information about the originally requested track.
+     */
+    @JsonProperty(value = "linked_from")
+    private TrackLink linkedFrom;
 
     /**
      * The popularity of the track. The value will be between 0 and 100, with 100
@@ -94,6 +103,12 @@ public class SpotifyTrack extends SpotifyElement {
     private int trackNumber;
 
     /**
+     * Whether or not the track is from a local file.
+     */
+    @JsonProperty(value = "is_local")
+    private boolean isLocal;
+
+    /**
      * Mapping for Spotify's external IDs.
      */
     @Data
@@ -107,5 +122,40 @@ public class SpotifyTrack extends SpotifyElement {
          * Code).
          */
         private String isrc;
+    }
+
+    /**
+     * Mapping for Spotify's linked from object.
+     */
+    @Data
+    @ToString
+    @JsonInclude(value = Include.NON_NULL)
+    private static final class TrackLink {
+
+        /**
+         * Known external URLs for this track.
+         */
+        @JsonProperty(value = "external_urls")
+        private ExternalUrls externalUrls;
+
+        /**
+         * A link to the Web API endpoint providing full details of the track.
+         */
+        private String href;
+
+        /**
+         * The Spotify ID for the track.
+         */
+        private String id;
+
+        /**
+         * The object type: “track”.
+         */
+        private String type;
+
+        /**
+         * The Spotify URI for the track.
+         */
+        private String uri;
     }
 }
