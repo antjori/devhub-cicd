af424b4d3b5ef6233a55f7c351bac1a5c1ecefce
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-11-21 08:01:43 +0000

    Add super class SpotifyPagingObject

--- src/main/java/pt/devhub/antjori/cicd/oac/model/spotify/SpotifyArtists.java
+++ src/main/java/pt/devhub/antjori/cicd/oac/model/spotify/SpotifyArtists.java
@@ -1,11 +1,10 @@
 package pt.devhub.antjori.cicd.oac.model.spotify;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 /**
@@ -13,42 +12,8 @@ import lombok.ToString;
  */
 @Data
 @ToString
+@EqualsAndHashCode(callSuper = true)
 @JsonInclude(value = Include.NON_NULL)
-public class SpotifyArtists {
+public class SpotifyArtists extends SpotifyPagingObject<SpotifyArtist> {
 
-    /**
-     * A link to the Web API endpoint returning the full result of the request.
-     */
-    private String href;
-
-    /**
-     * The requested data.
-     */
-    List<SpotifyArtist> items;
-
-    /**
-     * The maximum number of items in the response (as set in the query or by
-     * default).
-     */
-    private int limit;
-
-    /**
-     * URL to the next page of items. (null if none)
-     */
-    private String next;
-
-    /**
-     * The offset of the items returned (as set in the query or by default).
-     */
-    private int offset;
-
-    /**
-     * URL to the previous page of items. (null if none)
-     */
-    private int previous;
-
-    /**
-     * The total number of items available to return.
-     */
-    private int total;
 }
