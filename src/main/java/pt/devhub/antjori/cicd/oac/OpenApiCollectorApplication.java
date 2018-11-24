25fa7ba5a8d68901a5584e888cb4ece17488e975
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-11-24 00:21:24 +0000

    Add OAC controller ITs

--- src/main/java/pt/devhub/antjori/cicd/oac/OpenApiCollectorApplication.java
+++ src/main/java/pt/devhub/antjori/cicd/oac/OpenApiCollectorApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * The bootstrap (base) application that will trigger Spring Boot.
  */
 @SpringBootApplication(scanBasePackages = "pt.devhub.antjori.cicd.oac")
-public class Application {
+public class OpenApiCollectorApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).run(args);
+        new SpringApplicationBuilder(OpenApiCollectorApplication.class).run(args);
     }
 }
---
+++ src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1IT.java
@@ -0,0 +1,53 @@
+package pt.devhub.antjori.cicd.oac.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import pt.devhub.antjori.cicd.oac.OpenApiCollectorApplication;
+
+/**
+ * Test class for {@link OpenApiCollectorControllerV1} where will be depicted
+ * the integration tests.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = OpenApiCollectorApplication.class)
+@AutoConfigureMockMvc
+public class OpenApiCollectorControllerV1IT {
+
+    /**
+     * Common request mapping URL path.
+     */
+    private static final String REQUEST_MAPPING = "/oac/v1";
+
+    @Autowired
+    private MockMvc mvc;
+
+    // =======
+    // SPOTIFY
+    // =======
+
+    @Test
+    public void testSearchSpotify() throws Exception {
+        // given
+        MockHttpServletRequestBuilder get = get(REQUEST_MAPPING + "/api/spotify").param("q", "Eminem").param("type",
+                "artist");
+
+        // when
+        ResultActions resultActions = mvc.perform(get).andDo(print());
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+}
