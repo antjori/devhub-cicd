8199f8dea070379447117fbb0fd9d46d14182283
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-11-24 01:34:52 +0000

    Add @TestPropertySource notation to IT

--- src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1IT.java
+++ src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1IT.java
@@ -7,11 +7,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -27,6 +29,7 @@ import pt.devhub.antjori.cicd.oac.OpenApiCollectorApplication;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = OpenApiCollectorApplication.class)
 @AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.yml")
 @ActiveProfiles(value = "test")
 public class OpenApiCollectorControllerV1IT {
 
@@ -34,6 +37,12 @@ public class OpenApiCollectorControllerV1IT {
      * Common request mapping URL path.
      */
     private static final String REQUEST_MAPPING = "/oac/v1";
+
+    @Value(value = "${security.user.name}")
+    private String user;
+
+    @Value(value = "${security.user.password}")
+    private String password;
 
     @Autowired
     private MockMvc mvc;
@@ -47,7 +56,7 @@ public class OpenApiCollectorControllerV1IT {
         // given
         MockHttpServletRequestBuilder get = get(REQUEST_MAPPING + "/api/spotify")
                 .header(HttpHeaders.AUTHORIZATION,
-                        "Basic " + Base64Utils.encodeToString("oac-user:password".getBytes()))
+                        "Basic " + Base64Utils.encodeToString((user + ":" + password).getBytes()))
                 .param("q", "Eminem").param("type", "artist");
 
         // when
