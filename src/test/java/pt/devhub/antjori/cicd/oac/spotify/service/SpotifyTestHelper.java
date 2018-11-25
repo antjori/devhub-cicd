b87506d1ad214bc7cf843c4cd9d0d89119a969b0
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-11-25 23:55:26 +0000

    Add Spotify object creation methods

--- src/test/java/pt/devhub/antjori/cicd/oac/spotify/service/SpotifyTestHelper.java
+++ src/test/java/pt/devhub/antjori/cicd/oac/spotify/service/SpotifyTestHelper.java
@@ -1,7 +1,11 @@
 package pt.devhub.antjori.cicd.oac.spotify.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 
 import pt.devhub.antjori.cicd.oac.spotify.model.album.SpotifyAlbum;
 import pt.devhub.antjori.cicd.oac.spotify.model.artist.SpotifyArtist;
@@ -11,18 +15,35 @@ import pt.devhub.antjori.cicd.oac.spotify.model.artist.SpotifyArtist;
  */
 public final class SpotifyTestHelper {
 
-    public static final SpotifyAlbum createSpotifyAlbum() {
+    @Value(value = "${oac.spotify.availableMarkets}")
+    private List<String> availableMarkets;
+
+    public final SpotifyAlbum createSpotifyAlbum() {
         SpotifyAlbum album = new SpotifyAlbum();
 
         album.setType("album");
         album.setArtists(createSpotifyArtists());
+        album.setAvailableMarkets(createAvailableMarkets());
+        album.setExternalUrls(createExternalUrls());
 
         return album;
     }
 
-    public static final List<SpotifyArtist> createSpotifyArtists() {
+    public final List<SpotifyArtist> createSpotifyArtists() {
         List<SpotifyArtist> artists = new ArrayList<>();
 
         return artists;
+    }
+
+    public final List<String> createAvailableMarkets() {
+        return this.availableMarkets;
+    }
+
+    public final Map<String, String> createExternalUrls() {
+        Map<String, String> externalUrls = new HashMap<>();
+
+        externalUrls.put("spotify", "https://open.spotify.com/album/3HNnxK7NgLXbDoxRZxNWiR");
+
+        return externalUrls;
     }
 }
