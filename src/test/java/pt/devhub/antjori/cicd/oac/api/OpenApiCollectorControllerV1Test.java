47ac87ad068b29529c598e4929eb474b4b560c60
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-12-03 07:54:14 +0000

    Add unit test classes for OAC controller and Spotify service

---
+++ src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1Test.java
@@ -0,0 +1,22 @@
+package pt.devhub.antjori.cicd.oac.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Test class for {@link OpenApiCollectorControllerV1} where will be depicted
+ * the classe's unit tests.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OpenApiCollectorControllerV1Test {
+
+    @InjectMocks
+    private OpenApiCollectorControllerV1 oacController;
+
+    @Test
+    public void testSearchSpotify() {
+        // Implement me
+    }
+}
---
+++ src/test/java/pt/devhub/antjori/cicd/oac/spotify/service/SpotifyServiceTest.java
@@ -0,0 +1,22 @@
+package pt.devhub.antjori.cicd.oac.spotify.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Test class for {@link SpotifyService} where will be depicted the classe's
+ * unit tests.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SpotifyServiceTest {
+
+    @InjectMocks
+    private SpotifyService spotifyService;
+
+    @Test
+    public void testSearch() {
+        // Implement me
+    }
+}
