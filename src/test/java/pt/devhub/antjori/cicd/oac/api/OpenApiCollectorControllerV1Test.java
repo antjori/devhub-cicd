cc3f12686697e089ca64443a1fb276b4a934419e
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-12-04 23:54:05 +0000

    Continue test case for Spotify search

--- src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1Test.java
+++ src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1Test.java
@@ -1,11 +1,14 @@
 package pt.devhub.antjori.cicd.oac.api;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
 
+import pt.devhub.antjori.cicd.oac.spotify.model.response.SpotifySearchResponse;
 import pt.devhub.antjori.cicd.oac.spotify.service.SpotifyService;
 
 /**
@@ -21,8 +24,18 @@ public class OpenApiCollectorControllerV1Test {
     @Mock
     private SpotifyService spotifyService;
 
+    @Before
+    public void setup() {
+        // MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void testSearchSpotify() {
-        // Implement me
+        // given
+
+        // when
+        ResponseEntity<SpotifySearchResponse> response = oacController.searchSpotify("Eminem", "album,artist,track");
+
+        // then
     }
 }
