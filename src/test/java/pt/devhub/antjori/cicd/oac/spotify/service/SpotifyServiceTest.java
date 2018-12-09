dec5c56b06ec215bfbd84b84af799ea2b0608f58
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-12-09 22:51:39 +0000

    Continue Spotify service tests

--- src/test/java/pt/devhub/antjori/cicd/oac/spotify/service/SpotifyServiceTest.java
+++ src/test/java/pt/devhub/antjori/cicd/oac/spotify/service/SpotifyServiceTest.java
@@ -1,5 +1,9 @@
 package pt.devhub.antjori.cicd.oac.spotify.service;
 
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 import pt.devhub.antjori.cicd.oac.spotify.model.ClientCredentials;
@@ -30,15 +35,23 @@ public class SpotifyServiceTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+
+        when(clientCredentials.getAccessToken())
+                .thenReturn("BQBkcbGl9poui2Cp-_2S3P_n2gA1-ImfTqyI2SFzc8sKa7q0tKfcqipake2f46C4bZXp40kZnSus-Ajey4E");
     }
 
     @Test
     public void testSearch_withoutCredentials() {
         // given
+        when(restTemplate.getInterceptors()).thenReturn(new ArrayList<ClientHttpRequestInterceptor>());
 
         // when
 
         // then
+//        verify(restTemplate, times(1)).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class),
+//                ClientCredentials.class);
+//        verify(restTemplate, times(1)).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class),
+//                SpotifySearchResponse.class, anyString(), anyString());
     }
 
     @Test
@@ -48,5 +61,7 @@ public class SpotifyServiceTest {
         // when
 
         // then
+//        verify(restTemplate, times(1)).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class),
+//                any(SpotifySearchResponse.class), anyString(), anyString());
     }
 }
