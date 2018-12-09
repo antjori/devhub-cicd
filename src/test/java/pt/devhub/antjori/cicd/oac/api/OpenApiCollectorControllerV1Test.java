0c5b1971a7f06b7e7de87f7c2381d97b8f1c955c
antjori <antonio.romaneiro.ribeiro@gmail.com>
2018-12-09 21:10:38 +0000

    Complete OAC controller unit test

--- src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1Test.java
+++ src/test/java/pt/devhub/antjori/cicd/oac/api/OpenApiCollectorControllerV1Test.java
@@ -1,14 +1,25 @@
 package pt.devhub.antjori.cicd.oac.api;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 
+import pt.devhub.antjori.cicd.oac.spotify.model.album.SpotifyAlbums;
+import pt.devhub.antjori.cicd.oac.spotify.model.artist.SpotifyArtists;
 import pt.devhub.antjori.cicd.oac.spotify.model.response.SpotifySearchResponse;
+import pt.devhub.antjori.cicd.oac.spotify.model.track.SpotifyTracks;
 import pt.devhub.antjori.cicd.oac.spotify.service.SpotifyService;
 
 /**
@@ -26,16 +37,84 @@ public class OpenApiCollectorControllerV1Test {
 
     @Before
     public void setup() {
-        // MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testSearchSpotify() {
         // given
+        SpotifySearchResponse spotifySearchResponse = new SpotifySearchResponse();
+        spotifySearchResponse.setAlbums(new SpotifyAlbums());
+        spotifySearchResponse.setArtists(new SpotifyArtists());
+        spotifySearchResponse.setTracks(new SpotifyTracks());
+        when(spotifyService.search(anyString(), anyString())).thenReturn(spotifySearchResponse);
 
         // when
         ResponseEntity<SpotifySearchResponse> response = oacController.searchSpotify("Eminem", "album,artist,track");
 
         // then
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().getAlbums());
+        assertNotNull(response.getBody().getArtists());
+        assertNotNull(response.getBody().getTracks());
+        verify(spotifyService, times(1)).search(anyString(), anyString());
+    }
+
+    @Test
+    public void testSearchSpotify_withAlbums() {
+        // given
+        SpotifySearchResponse spotifySearchResponse = new SpotifySearchResponse();
+        spotifySearchResponse.setAlbums(new SpotifyAlbums());
+        when(spotifyService.search(anyString(), anyString())).thenReturn(spotifySearchResponse);
+
+        // when
+        ResponseEntity<SpotifySearchResponse> response = oacController.searchSpotify("Eminem", "album");
+
+        // then
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().getAlbums());
+        assertNull(response.getBody().getArtists());
+        assertNull(response.getBody().getTracks());
+        verify(spotifyService, times(1)).search(anyString(), anyString());
+    }
+
+    @Test
+    public void testSearchSpotify_withArtists() {
+        // given
+        SpotifySearchResponse spotifySearchResponse = new SpotifySearchResponse();
+        spotifySearchResponse.setArtists(new SpotifyArtists());
+        when(spotifyService.search(anyString(), anyString())).thenReturn(spotifySearchResponse);
+
+        // when
+        ResponseEntity<SpotifySearchResponse> response = oacController.searchSpotify("Eminem", "artist");
+
+        // then
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertNull(response.getBody().getAlbums());
+        assertNotNull(response.getBody().getArtists());
+        assertNull(response.getBody().getTracks());
+        verify(spotifyService, times(1)).search(anyString(), anyString());
+    }
+
+    @Test
+    public void testSearchSpotify_withTracks() {
+        // given
+        SpotifySearchResponse spotifySearchResponse = new SpotifySearchResponse();
+        spotifySearchResponse.setTracks(new SpotifyTracks());
+        when(spotifyService.search(anyString(), anyString())).thenReturn(spotifySearchResponse);
+
+        // when
+        ResponseEntity<SpotifySearchResponse> response = oacController.searchSpotify("Eminem", "track");
+
+        // then
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertNull(response.getBody().getAlbums());
+        assertNull(response.getBody().getArtists());
+        assertNotNull(response.getBody().getTracks());
+        verify(spotifyService, times(1)).search(anyString(), anyString());
     }
 }
